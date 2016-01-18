b2da909df520539dd3321dac85ec1503b5c507b4
naka-k <kagician0319@gmail.com>
2016-01-19 01:14:22 +0900

    Add KN_Custom

---
+++ KN_Custom/KN_Custom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KN_Custom
+//
+//  Created by 中克之 on 2016/01/19.
+//  Copyright © 2016年 中克之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
